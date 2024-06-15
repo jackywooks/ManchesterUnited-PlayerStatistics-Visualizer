3146fc79b54d3413d8b6ff5a05fc863ddadcbb34
jackywooks <146035892+jackywooks@users.noreply.github.com>
2024-06-15 11:20:40 -0400

    Add Rating PieChart
    - layout update, able to switch between pie chart and bar chart

--- Data Visualizer/src/main/java/org/example/dataVisualizer/PlayerChartController.java
+++ Data Visualizer/src/main/java/org/example/dataVisualizer/PlayerChartController.java
@@ -1,13 +1,17 @@
 package org.example.dataVisualizer;
 //import Statement
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
+import javafx.scene.chart.PieChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.kordamp.bootstrapfx.BootstrapFX;
@@ -17,6 +21,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Comparator;
 
 /**
  * @author      Jacky Woo jackywooksca@gmail.com
@@ -25,30 +30,38 @@ import java.sql.Statement;
  */
 
 public class PlayerChartController {
+    @FXML
+    private Label title;
     /**
-     * Initialize the Chart and the column variables.
+     * Initialize the Bar Chart and the column variables.
+     * Create a XYChart Series to store the player name and goals
      */
     @FXML
     private BarChart<String,Number> goalLeaderboard;
-
-    /**
-     * Create a XYChart Series to store the player name and goals
-     */
     private XYChart.Series<String,Number> playerGoalSeries = new XYChart.Series<String,Number>();
 
     /**
-     * Initialize the Chart and link with the playerSeries
+     * Initialize the Pie Chart.
+     * Create an Observable List to store the player name and ratings
+     */
+    @FXML
+    private PieChart ratingBoard;
+    private ObservableList<PieChart.Data> ratingData = FXCollections.observableArrayList();
+
+    /**
+     * Initialize the scene with goal leader board
      */
     @FXML
     private void initialize(){
-        showGoalLeaderboard();
+        fetchGoalLeaderboard();
+        fetchRatingChart();
     }
 
     /**
-     * To retrieve data from DB and display in the TableView
+     * To retrieve data from DB and display in the Bar Chart
      */
     @FXML
-    private void showGoalLeaderboard(){
+    private void fetchGoalLeaderboard(){
         DatabaseConnector dbConnector = new DatabaseConnector();
         try (Connection connection = dbConnector.connect()) {
             Statement statement = connection.createStatement();
@@ -58,14 +71,69 @@ public class PlayerChartController {
                 String name = resultSet.getString("player_name");
                 Integer goal = resultSet.getInt("goal");
                 //Store the data in a XY Series
+                playerGoalSeries.setName("Goals");
                 playerGoalSeries.getData().add(new XYChart.Data<String,Number>(name,goal));
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
+        //Sort the data by Goals
+        playerGoalSeries.getData().sort(Comparator.comparingInt(d->d.getYValue().intValue()));
+        //put XY series data in the goalLeaderboard bar chart
         goalLeaderboard.getData().add(playerGoalSeries);
+        goalLeaderboard.setStyle("CHART_COLOR_1: #ff0000;");
     }
+    /**
+     * To retrieve data from DB and display in the Pie Chart
+     */
+    @FXML
+    private void fetchRatingChart(){
+        DatabaseConnector dbConnector = new DatabaseConnector();
+        try (Connection connection = dbConnector.connect()) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("" +
+                    "SELECT Category, COUNT(*) AS PlayerCount\n" +
+                    "FROM (\n" +
+                    "  SELECT \n" +
+                    "    CASE WHEN Rating > 7.5 THEN 'Elite - Greater Than 7.5'\n" +
+                    "         WHEN Rating > 6.75 THEN 'Nice - Greater Than 6.75'\n" +
+                    "         ELSE 'Mediocre - 6.75 or Lower'\n" +
+                    "    END AS Category\n" +
+                    "  FROM playerStat\n" +
+                    ") AS categorized_players\n" +
+                    "GROUP BY Category;");
+            while (resultSet.next()) {
+                //Store the result from SQL query to variables
+                String ratingGrade = resultSet.getString("Category");
+                Double playerCount = resultSet.getDouble("PlayerCount");
+                //Store the data in a XY Series
+                ratingData.add(new PieChart.Data(ratingGrade,playerCount));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        ratingBoard.setData(ratingData);
+        //Default Hiding the rating board
+        ratingBoard.setVisible(false);
+    }
+    /**
+     * To retrieve data from DB and display in the Pie Chart
+     */
+    @FXML
+    private void changeToRatingBoard(){
+        title.setText("Player's Rating Board");
+        goalLeaderboard.setVisible(false);
+        ratingBoard.setVisible(true);
+    }
+
+    @FXML
+    private void changeToGoalBoard(){
+        title.setText("Goal Scorer Board");
+        goalLeaderboard.setVisible(true);
+        ratingBoard.setVisible(false);
+    }
+
     /**
      * Switch the scene to Table View, by loading the "player-stat.fxml", retrieve the current stage, and switch to the loaded fxml
      *
